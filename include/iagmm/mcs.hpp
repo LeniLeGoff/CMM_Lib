d9957b825e4e9a431f8f207bbead0ce1b33f3fe4
Le Goff <le_goff@isir.upmc.fr>
2017-09-04 18:07:29 +0200

    modif sampling method

--- include/iagmm/mcs.hpp
+++ include/iagmm/mcs.hpp
@@ -2,6 +2,7 @@
 #define MCS_HPP
 
 #include <iagmm/mcs_fct.hpp>
+#include <eigen3/Eigen/IterativeLinearSolvers>
 
 
 namespace iagmm
@@ -9,18 +10,21 @@ namespace iagmm
 class MCS : public Classifier {
 
 public:
-    MCS(comb_fct_t& cmb, opt_fct_t& opt):
-        _comb_fct(cmb),_opt_fct(opt){
+
+    MCS(std::vector<Classifier::Ptr>& class_list,const comb_fct_t& cmb):
+        _classifiers(class_list), _comb_fct(cmb){
+        _dimension = _classifiers[0]->get_dimension();
+        _nbr_class = _classifiers[0]->get_nbr_class();
+        _parameters = Eigen::VectorXd::Constant(class_list.size(),1.);
     }
-    MCS(std::vector<Classifier::Ptr>& class_list, comb_fct_t& cmb, opt_fct_t& opt):
-        _classifiers(class_list), _comb_fct(cmb),_opt_fct(opt){    }
     MCS(const MCS& mcs) :
         Classifier(mcs), _parameters(mcs._parameters),
-        _classifiers(mcs._classifiers), _comb_fct(mcs._comb_fct),
-        _opt_fct(mcs._opt_fct){}
-
+        _classifiers(mcs._classifiers), _comb_fct(mcs._comb_fct){
+    }
 
     double compute_estimation(const Eigen::VectorXd &sample, int lbl);
+
+    void add(const Eigen::VectorXd &sample, int lbl);
 
     void update();
 
@@ -29,8 +33,6 @@ private:
     std::vector<Classifier::Ptr> _classifiers;
     Eigen::MatrixXd _estimations;
     comb_fct_t _comb_fct;
-    opt_fct_t _opt_fct;
-
 };
 }
 
--- src/mcs.cpp
+++ src/mcs.cpp
@@ -4,6 +4,8 @@
 
 using namespace iagmm;
 
+const std::map<std::string,comb_fct_t> combinatorial::fct_map = combinatorial::create_map();
+
 double MCS::compute_estimation(const Eigen::VectorXd& sample, int lbl){
     std::vector<double> estimations;
 
@@ -13,20 +15,40 @@ double MCS::compute_estimation(const Eigen::VectorXd& sample, int lbl){
     return _comb_fct(_parameters,estimations);
 }
 
-void MCS::update(){
-    _estimations = Eigen::MatrixXd::Zero(_classifiers.size(),_samples.size());
-    Eigen::VectorXd lbl(_samples.size());
+void MCS::add(const Eigen::VectorXd &sample, int lbl){
+    _samples.add(lbl,sample);
+    for(auto& c : _classifiers)
+        c->add(sample,lbl);
 
-    tbb::parallel_for(tbb::blocked_range2d<size_t>(0,_classifiers.size(),0,_samples.size()),
+}
+
+void MCS::update(){
+
+    for(auto& c : _classifiers)
+        c->update();
+
+    int c_size = _classifiers.size();
+    int s_size = _samples.size();
+
+    _estimations = Eigen::MatrixXd::Zero(s_size,c_size);
+    Eigen::VectorXd lbl(s_size);
+
+
+    tbb::parallel_for(tbb::blocked_range2d<size_t>(0,c_size,0,s_size),
                       [&](const tbb::blocked_range2d<size_t>& r ){
         for(size_t i = r.rows().begin(); i != r.rows().end(); i++)
             for(size_t j = r.cols().begin(); j != r.cols().end(); j++)
-                _estimations(i,j) = _classifiers[i]->compute_estimation(_samples[j].second,_samples[j].first);
+                _estimations(j,i) = _classifiers[i]->compute_estimation(_samples[j].second,_samples[j].first);
     });
 
-    for(int i = 0; i < _samples.size(); i++){
-        lbl(i) = _samples[i].first;
-    }
 
-    _opt_fct(_parameters,_estimations,lbl);
+//    for(int i = 0; i < c_size; i++){
+//        lbl(i) = _samples[i].first;
+//    }
+
+//    _conjugate_grad.compute(_estimations);
+//    _parameters = _conjugate_grad.solve(lbl);
+//    std::cout << _parameters << std::endl;
+
+//    std::cout <<  _conjugate_grad.iterations() << " error : " << _conjugate_grad.error() << std::endl;
 }
