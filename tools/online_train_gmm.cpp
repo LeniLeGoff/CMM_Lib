4ad4d15687ed0ee09d6e78ee43f860a5b6409066
leni le goff <lenilegoff@gmail.com>
2018-01-31 11:53:34 +0100

    cleaning online_train_gmm

--- tools/online_train_gmm.cpp
+++ tools/online_train_gmm.cpp
@@ -13,40 +13,40 @@
 int main(int argc, char** argv){
 
     if(argc =! 3){
-        std::cerr << "Usage : yaml file with dataset, dataset_max_size" << std::endl;
+        std::cerr << "Usage : yaml file with dataset, output file, (optional) dataset max size" << std::endl;
         return 1;
     }
-//    srand(std::time(NULL));
+    bool with_update_dataset = argc == 4;
+
 
     int dimension, nbr_class;
     iagmm::TrainingData dataset;
     dataset.load_yml(argv[1],dimension,nbr_class);
-    int dataset_max_size = std::stoi(argv[2]);
+
 
     iagmm::GMM gmm(dimension,nbr_class);
-//    gmm.set_dataset_size_max(dataset_max_size);
-
-//    boost::random::mt19937 gen;
-//    gen.seed(rand());
+    if(with_update_dataset)
+        gmm.set_dataset_size_max(std::stoi(argv[3]));
 
 
-//    int index;
+
+
     for(int i = 0; i < dataset.size(); i++){
-//        boost::random::uniform_int_distribution<> dist(0,dataset.size());
-//        index = dist(gen);
+
         std::cout << "_____________________________________" << std::endl;
         std::cout << "iteration number : " << i << std::endl;
         std::cout << "total number of samples : " << gmm.number_of_samples() << std::endl;
         std::cout << std::endl;
         gmm.add(dataset[i].second,dataset[i].first);
         gmm.update();
-        gmm.update_dataset();
+        if(with_update_dataset)
+            gmm.update_dataset();
         std::cout << gmm.print_info() << std::endl;
     }
 
     boost::filesystem::path file_path(argv[1]);
     std::stringstream ss;
-    ss << file_path.parent_path().c_str() << "/online_learned_gmm";
+    ss << file_path.parent_path().c_str() << argv[2];
     std::ofstream ofs(ss.str());
     boost::archive::text_oarchive toa(ofs);
     toa << gmm;
